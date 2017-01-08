959253c6641572a072ff16ea615b1d43bae54309
Matija Folnovic <mfolnovic@gmail.com>
2017-01-08 21:02:53 +0100

    Fill LMS in buckets

--- saca-k.cpp
+++ saca-k.cpp
@@ -20,6 +20,28 @@ void initializeBuckets(char* T, int* bkt, int K, int n) {
     }
 }
 
+void initializeSA(int* SA, int n) {
+    memset(SA, 0, sizeof(int) * n);
+}
+
+void induceSortLMS(char* T, int* SA, int* bkt, int K, int n) {
+    bool current_s_type = false;
+
+    for (int i = n-2; i > 0; i--) {
+        bool previous_s_type = T[i - 1] < T[i] || (T[i - 1] == T[i] && current_s_type);
+
+        // if LMS
+        if (!previous_s_type && current_s_type) {
+            SA[bkt[T[i]]] = i;
+            bkt[T[i]] -= 1;
+        }
+
+        current_s_type = previous_s_type;
+    }
+
+    SA[0] = n - 1;
+}
+
 void sacak(char* T, int* SA, int K, int n, int level) {
     int *bkt = NULL;
 
@@ -29,9 +51,23 @@ void sacak(char* T, int* SA, int K, int n, int level) {
         // initialize buckets to end of each bucket
         initializeBuckets(T, bkt, K, n);
 
+        // initialize each item of SA[0, N-1] as empty
+        initializeSA(SA, n);
+
+        // compute into bkt[0, K-1] the end position of each bucket in SA. Scan
+        // SA[0, N-1] once from right to left to put all the sorted LMS-suffixes
+        // of T into their buckets in SA, from the end to the start in each
+        // bucket.
+        induceSortLMS(T, SA, bkt, K, n);
+
         printf("%d\n", K);
         for (int i = 0; i < K; i++) {
             printf("%d ", *(bkt + i));
+        }
+        printf("\n\n");
+
+        for (int i = 0; i < n; i++) {
+            printf("%d ", SA[i]);
         }
         printf("\n");
     } else {
